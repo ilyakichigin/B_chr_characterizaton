f8df356af8b65c75d1b95f091cfb8144778d0c10
Ilya <kig@mcb.nsc.ru>
2015-06-10 13:48:49 +0500

    Update get_hom.py

--- anolis/exec/get_hom.py
+++ anolis/exec/get_hom.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 
-from collections import namedtuple, defaultdict, Counter, OrderedDict
+from collections import namedtuple, defaultdict, OrderedDict
 import sys
 import argparse
 
@@ -20,23 +20,23 @@ def parse_command_line_arguments():
 if __name__ == '__main__':
 	args = parse_command_line_arguments()
 
-	File = args.net_file
-
-	TopNet = defaultdict(list)
-	with open(File) as f:
-		for Line in f:
-			LL = Line.split()
+	top_net = defaultdict(list)
+	with open(args.net_file) as input_file:
+		for line in input_file:
+			LL = line.split()
 			if LL[0] == 'net': # new chromosome
-				T_chr = LL[1]
+				t_chr = LL[1]
 			elif LL[16] == 'top': # top level of net
-				TopNet[T_chr].append( [LL[3],LL[10]] ) # Q_chr, alignment score
-	for (T_chr,Fill) in TopNet.items():
-		AlnScores = defaultdict(int)
-		for F in Fill:
-			AlnScores[F[0]] += int(F[1]) # sum up scores for Q_chr
-		SAS = OrderedDict(sorted(AlnScores.items(), key=lambda t: t[1], reverse=True)) # sort by score
+				top_net[t_chr].append( [LL[3],LL[10]] ) # Q_chr, alignment score
+	for (t_chr,fill) in top_net.items():
+		aln_scores = defaultdict(int)
+		for F in fill:
+			aln_scores[F[0]] += int(F[1]) # sum up scores for Q_chr
+		SAS = OrderedDict(sorted(aln_scores.items(), key=lambda t: t[1], reverse=True)) # sort by score
 		SAL = []
 		for L in SAS.items():
 			SAL+=list(L)
 		SAL = [str(L) for L in SAL]
-		print T_chr + '\t' +','.join(SAL)
+		print t_chr + '\t' +','.join(SAL)
+
+	sys.stderr.write("Complete!\n")
