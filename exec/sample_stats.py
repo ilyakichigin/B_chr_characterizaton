847d15803d313b5a46b5e3e27915b50cb31c8dce
Drevoz <alex.makunin@gmail.com>
2017-07-17 15:39:53 +0700

    sample stats from logs, pipeline fitting and description updates

--- exec/sample_stats.py
+++ exec/sample_stats.py
@@ -4,9 +4,7 @@ import subprocess
 import tempfile
 import argparse
 import pysam
-import gzip
 import sys
-import re
 import os
 
 def parse_command_line_arguments():
@@ -16,72 +14,22 @@ def parse_command_line_arguments():
                     """
                     )
     parser.add_argument("sample", help="sample name - used as prefix")
-    parser.add_argument("F_reads", help="forward reads .fastq")
-    parser.add_argument("R_reads", help="reverse reads .fastq")
-    parser.add_argument("-r", "--regions", default=None, help="verified target regions .bed")
+    parser.add_argument("genome", help="target genome prefix")
+    parser.add_argument("-f","--f_reads", default='', help="forward reads .fastq[.gz]")
+    parser.add_argument("-r","--r_reads", default='', help="reverse reads .fastq[.gz]")
+    parser.add_argument("-t", "--target_regions", default=None, help="verified target regions .bed")
+    parser.add_argument("-o", "--output", default=None, help="Output filename. default <sample>.stats.txt")
 
     return parser.parse_args()
 
-def get_filenames(sample):
+def generate_filenames(sample, genome):
 
     names = dict()
-    names['f_trim_fq'] = sample + '.ca.R1.fastq'
-    names['r_trim_fq'] = sample + '.ca.R2.fastq'
-
-    fb_names = [f for f in os.listdir('.') if re.match(r'.*\.filter\.bam$', f)]
-    if len(fb_names) < 2:
-        names['filtered_bam'] = ''.join(fb_names)
-    else:
-        raise Exception('Too many *filter.bam files in current folder %s' % str(fb_names))
-
-    cb_names = [f for f in os.listdir('.') if re.match(r'.*\.contam\.bam$', f)]
-    if len(cb_names) < 2:
-        names['contam_bam'] = ''.join(cb_names)
-    else:
-        raise Exception('Too many *contam.bam files in current folder %s' % str(cb_names))
-
-    pb_names = [f for f in os.listdir('.') if re.match(r'.*\.pos\.bed$', f)]
-    if len(pb_names) < 2:
-        names['pos_bed'] = ''.join(pb_names[0])
-    else:
-        raise Exception('Too many *pos.bed files in current folder')
+    names['ca_log'] = '.'.join([sample,'ca','log'])
+    names['filter_log'] = '.'.join([sample,genome,'filter','log'])
+    names['pos_bed'] = '.'.join([sample,genome,'pos','bed'])
 
     return names
-
-def fastq_stats(fastq_name):
-
-    i = 0
-    readlen = 0
-    
-    if fastq_name.endswith('.fastq'):
-        f = open(fastq_name)
-    elif fastq_name.endswith('.fastq.gz'):
-        f = gzip.open(fastq_name)
-    else:
-        raise Exception('Improper read naming: %s' % (fastq_name))
-
-    for line in f:
-        i += 1
-        if i%4 == 1 and not line.startswith('@'):
-            raise Exception('Corrupt read file: %s' % (fastq_name))
-        if i%4 == 2:
-            readlen += len(line.strip('\n'))
-    f.close()
-
-    return [i/4, readlen]
-
-def bam_stats(filename):
-
-    n_reads = 0
-    if filename != '':
-        # from https://www.biostars.org/p/1890/ - get number of mapped reads
-        for l in pysam.idxstats(filename).split('\n'):
-            ll = l.rstrip('\n').split('\t')
-            if len(ll) == 4:
-                n_reads += int(ll[2])
-        return n_reads
-    else:
-        return 'NA'
 
 def bed_stats(f):
 
@@ -96,38 +44,56 @@ def bed_stats(f):
         if len(ll) > 3:
             read_num += int(ll[3])
 
-    return(i, pos_size, float(read_num)/i)
+    return(str(i), str(pos_size), str(float(read_num)/i), str(float(pos_size)/i))
+
+def del_1000_sep(s):
+    return ''.join(s.split(','))
 
 def main(args):
     
-    names = get_filenames(args.sample)
+    names = generate_filenames(args.sample, args.genome)
+    # collect stats as list of tuples
     stats = [('sample',args.sample)]
 
-    # init fastq stats
-    f_init_stat = fastq_stats(args.F_reads)
-    r_init_stat = fastq_stats(args.F_reads)
-    stats += ('Initial reads', str(f_init_stat[0] + r_init_stat[0])), ('Initial bp', str(f_init_stat[1] + r_init_stat[1]))
-    # trimmed fastq stats
-    f_trim_stat = fastq_stats(names['f_trim_fq'])
-    r_trim_stat = fastq_stats(names['r_trim_fq'])
-    stats += ('Trimmed reads', str(f_trim_stat[0] + r_trim_stat[0])), ('Trimmed bp', str(f_trim_stat[1] + r_trim_stat[1]))
+    # fastq stats
+    with open(names['ca_log']) as f:
+        for line in f:
+            ll = line.split()
+            if line.startswith('Total read pairs processed'):
+                stats.append(('Total read pairs', del_1000_sep(ll[-1])))
+            elif line.startswith('Pairs written (passing filters)'):
+                stats.append(('Trimmed read pairs',del_1000_sep(ll[-2])))
+            elif line.startswith('Total reads processed'):
+                stats.append(('Total reads',del_1000_sep(ll[-1])))
+            elif line.startswith('Reads written (passing filters)'):
+                stats.append(('Trimmed reads',del_1000_sep(ll[-2])))
+            elif line.startswith('Total basepairs processed'):
+                stats.append(('Total bp',del_1000_sep(ll[-2])))
+            elif line.startswith('Total written (filtered):'):
+                stats.append(('Trimmed bp',del_1000_sep(ll[-3])))
 
-    # post-filtering bam stats
-    stats.append(('Contamination reads', str(bam_stats(names['contam_bam']))))
-    stats.append(('Target reads after filtering', str(bam_stats(names['filtered_bam']))))
+    with open(names['filter_log']) as f:
+        for line in f:
+            if line.startswith('Target aligned fragments'):
+                stats.append(('Aligned reads', line.split()[-1]))
+            elif line.startswith('Filtered Q'):
+                stats.append(('Filtered reads', line.split()[-1]))
+            elif line.startswith('Contamination Q'):
+                stats.append(('Contamination reads', line.split()[-1]))
+
     # pos file stats
     with open(names['pos_bed']) as f:
         pos_stat = bed_stats(f)
-        stats += ('Read positions', str(pos_stat[0])),('Position sum bp', str(pos_stat[1])),('Reads per position', str(pos_stat[2]))
-    stats.append(('Position mean bp', str(float(pos_stat[1])/pos_stat[0])))
+        stats += ('Read positions', pos_stat[0]),('Position sum bp', pos_stat[1])
+        stats += ('Reads per position', pos_stat[2]),('Position mean bp', pos_stat[3])
     # regpos stats
-    if args.regions is not None:
-        with open(args.regions) as reg_bed:
+    if args.target_regions is not None:
+        with open(args.target_regions) as reg_bed:
             reg_stat = bed_stats(reg_bed)
-            stats += ('Regions', str(reg_stat[0])),('Regions bp', str(reg_stat[1]))
+            stats += ('Regions', reg_stat[0]),('Regions bp', reg_stat[1])
 
         with tempfile.TemporaryFile() as regpos_bed:
-            bt_command = ['bedtools','intersect','-a',args.regions,'-b',names['pos_bed']]
+            bt_command = ['bedtools','intersect','-a',args.target_regions,'-b',names['pos_bed']]
             p = subprocess.Popen(bt_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             (out, err) = p.communicate()
             if p.returncode != 0:
@@ -135,10 +101,14 @@ def main(args):
             regpos_bed.write(out)
             regpos_bed.seek(0)
             regpos_stat = bed_stats(regpos_bed)
-            stats += ('Read positions in regions', str(regpos_stat[0])),('Position sum bp in regions', str(regpos_stat[1]))
-        stats.append(('Position in region/region bp', str(float(regpos_stat[1])/reg_stat[1])))
-        stats.append(('Position in region/genome positions bp', str(float(regpos_stat[1])/pos_stat[1])))
-    print '\n'.join(['\t'.join(s) for s in stats])
+            stats += ('Read positions in regions', regpos_stat[0]),('Position sum bp in regions', regpos_stat[1])
+            stats += ('Position in region/region bp', str(float(regpos_stat[1])/int(reg_stat[1])))
+            stats += ('Position in region/genome positions bp', str(float(regpos_stat[1])/int(pos_stat[1])))
+    #output
+    if args.output is None:
+        args.output = args.sample + '.stats.txt'
+    with open(args.output,'w') as f:
+        f.write('\n'.join(['\t'.join(s) for s in stats]))
 
 if __name__ == '__main__':
     args = parse_command_line_arguments()
--- pipeline/b_dopseq_pipe.py
+++ pipeline/b_dopseq_pipe.py
@@ -14,8 +14,7 @@ import fastq_clean
 import fastq_aln
 import contam_filter
 import bam_to_bed
-#import control_stats
-#import sample_stats
+import sample_stats
 
 def parse_command_line_arguments():
 
@@ -36,8 +35,8 @@ def parse_command_line_arguments():
 def generate_filenames(sample, target_path, contam_path):
 
     names = dict()
-    names['f_trim_fq'] = sample + '.ca.R1.fastq'
-    names['r_trim_fq'] = sample + '.ca.R2.fastq'
+    names['f_trim_fq'] = sample + '.ca.R1.fastq.gz'
+    names['r_trim_fq'] = sample + '.ca.R2.fastq.gz'
     names['target_path'] = target_path
     names['contam_path'] = contam_path
     names['target_name'] = target_path.split('/')[-1].split('.')[0]
@@ -50,6 +49,7 @@ def generate_filenames(sample, target_path, contam_path):
     names['pos_bed'] = '.'.join([sample,names['target_name'],'pos','bed'])
     names['reg_tsv'] = '.'.join([sample,names['target_name'],'reg','tsv'])
     names['reg_pdf'] = '.'.join([sample,names['target_name'],'reg','pdf'])
+    names['stat_file'] = conf['sample'] + '.stats.txt'
 
     return names
 
@@ -136,109 +136,15 @@ if __name__ == '__main__':
     else:
         sys.stderr.write('%s chromosome region prediction file exists. OK!\n' % fnames['reg_tsv'])
     sys.stderr.write('----Complete!----\n')
-'''
-    base_name = '.'.join([conf['sample'], target_name,'filter'])
-    filtered_bam_file = base_name + '.bam'
-    if not os.path.isfile(filtered_bam_file): 
 
-        # Step 1. fastq_clean if trimmed read fastq do not exists
-        # paired-end
-
-
-        
-
-        if "contam_genome" in conf.keys():  # contam filter
-            contam_name = conf["contam_genome"].split('/')[-1]
-            contam_name = contam_name.split('.')[0] + '_' + conf["aln"]
-            contam_sam_file = '.'.join([conf['sample'],contam_name,'sam'])
-            # Step 2b. Align to contamination genome
-            fb2c_args = argparse.Namespace(sample=conf['sample'],  aligner=conf["aln"], reference_genome=conf["contam_genome"], 
-                                         path_to_aligner=path_to_aligner[1:-1], aligner_args=aligner_args[1:-1])
-            fastq_to_bam.main(fb2c_args)
-            sys.stderr.write('----Complete!----\n')
-
-            # Step 3. contam_filter - remove contamination from the specified genome
-            cf_args = argparse.Namespace(target_file=target_sam_file, contam_file=contam_sam_file,
-                                         min_quality=conf['min_mapq'])
-            sys.stderr.write('----contam_filter.py----\n')
-            contam_filter.main(cf_args)
-            sys.stderr.write('----Complete!----\n')
-        else: # no contam filter
-            sys.stderr.write('Sorting and indexing %s, resulting file %s\n' % (target_sam_file, filtered_bam_file))
-            view_command = 'samtools view -bSq %s %s' % (conf['min_mapq'], target_sam_file)
-            sort_command = 'samtools sort - %s' % (filtered_bam_file[:-4])
-            sys.stderr.write('%s | %s\n' % (view_command, sort_command))
-            p1 = subprocess.Popen(view_command.split(), stdout=subprocess.PIPE)
-            p2 = subprocess.Popen(sort_command.split(), stdin=p1.stdout, stdout=subprocess.PIPE)
-            status = [p1.wait(),p2.wait()]
-            if p1.returncode != 0 or p1.returncode != 0:
-                sys.exit(1)
-            pysam.index(filtered_bam_file)
-            #os.remove(target_sam_file)
-            sys.stderr.write('----Complete!----\n')
-
-    # Step 4. Convert bam_to_beds with reads and positions if these files do not exist.
-    reads_bed_file = base_name + '.reads.bed'
-    pos_bed_file = base_name + '.pos.bed'
-    if (not os.path.isfile(reads_bed_file) and not os.path.isfile(pos_bed_file)):
-        btb_args = argparse.Namespace(bam_file=filtered_bam_file, path_to_bedtools='bedtools')
-        sys.stderr.write('----bam_to_beds.py----\n')
-        #try:
-        bam_to_beds.main(btb_args)
-        #except:
-        #    sys.exit(1)
-        sys.stderr.write('----Complete!----\n')
-    # Step 4a. Calculate control_stats if these do not exist.
-    stat_file = base_name + '.chrom.tsv'
-    if not os.path.isfile(stat_file):
-        cs_args = argparse.Namespace(bed_basename='.'.join([conf['sample'], target_name,'filter']),
-                                     sizes_file=conf['sizes_file'])
-        # redirect stdout to file and then return it back
-        sys.stderr.write('----control_stats.py----\n')        
-        saveout = sys.stdout        
-        with open(stat_file,'w') as f:
-            sys.stdout = f # replace stdout with file
-            #try:
-            control_stats.main(cs_args)
-            #except:
-            #    sys.exit(1)
-            sys.stdout = saveout # return std
-        sys.stderr.write('----Complete!----\n')
-    # Step 4b. Draw control_plots.R if these do not exist.
-    control_plot_file = base_name + '.chrom.pdf'
-    if not os.path.isfile(control_plot_file):
-        cp_command = [exec_path + '/control_plots.R', pos_bed_file,conf['sizes_file']]
-        sys.stderr.write('----control_plots.R----\n')
-        run_script(cp_command)
-        sys.stderr.write('----Complete!----\n')
-    # Step 5. Perform region_dnacopy.R if regions do not exist.
-    regions_table = base_name + '.reg.tsv'
-    regions_plot = base_name + '.reg.pdf'
-    if not os.path.isfile(regions_table) or not os.path.isfile(regions_plot):
-        # pdf height and width increased to get readable plot for all chromosomes 
-        rd_command = [exec_path + '/region_dnacopy.R', pos_bed_file,conf['sizes_file'], '20', '20']
-        sys.stderr.write('----region_dnacopy.R----\n')
-        run_script(rd_command)
-        sys.stderr.write('----Complete!----\n')
-    
-    # Step 6. Calculate sample stats if these do not exist. Less detailed than control_stats.
-    stat_file = conf['sample'] + '.stats.txt'
-    if not os.path.isfile(stat_file):
-        
-        if "contam_genome" in conf.keys():
-            contam_name = conf["contam_genome"].split('/')[-1]
-            ss_args = argparse.Namespace(sample=conf['sample'],
-                            F_reads=conf['fastq_F_file'], R_reads=conf['fastq_R_file'],
-                            t_genome=target_name,c_genome=contam_name)
-        else:
-            ss_args = argparse.Namespace(sample=conf['sample'],
-                            F_reads=conf['fastq_F_file'], R_reads=conf['fastq_R_file'],
-                            t_genome=target_name,c_genome='')
-        sys.stderr.write('----sample_stats.py----\n')
+    # Calculate sample stats if these do not exist. Less detailed than control_stats.
+    sys.stderr.write('----sample_stats.py----\n')
+    if not os.path.isfile(fnames['stat_file']):
+        sys.stderr.write('Writing stats to %s\n' % fnames['stat_file'])
+        ss_args = argparse.Namespace(sample=conf['sample'],genome=fnames['target_name'],
+                            f_reads=conf['fastq_F_file'], r_reads=conf['fastq_R_file'], 
+                            target_regions=None, output=None)
         sample_stats.main(ss_args)
-        #try:
-            
-        #except:
-        #    sys.exit(1)
-        sys.stderr.write('----Complete!----\n')
-    '''
+    else:
+        sys.stderr.write('%s statistics file exist. OK!\n' % fnames['stat_file'])
+    sys.stderr.write('----Complete!----\n')
