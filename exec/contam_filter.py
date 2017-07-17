d16835a30f95f40107930846d58076dd2a897a44
Drevoz <alex.makunin@gmail.com>
2017-07-17 15:37:42 +0700

    Enable logging, report all contaminant reads with high-quality amppings, disable samtools 0* support as not working

--- exec/contam_filter.py
+++ exec/contam_filter.py
@@ -41,16 +41,28 @@ def bam_sort(in_file, out_file, name_sort=False):
     else:
         raise Exception('Unrecognized samtools verion: %s. Supported versions: 1.*.*.' % (pysam.version.__samtools_version__[0]))
 
-def main(args):
+def compare_mapqs(treads, creads, filter_file, min_qual, i, j):
+    if sum([x.mapping_quality for x in treads])/len(treads) >= sum([x.mapping_quality for x in creads])/len(creads):
+        for read in treads:
+            if read.mapping_quality >= min_qual: # filter
+                i += 1
+                filter_file.write(read)
+    else:
+        for read in creads:
+            if read.mapping_quality >= min_qual: # contam
+                j += 1
 
+    return (i, j)
+
+def main(args):
     # output: filtered bam alignment
     filter_bam = args.target_bam[:-4] + '.filter.bam'
+    log_file = args.target_bam[:-4] + '.filter.log'
     if not os.path.isfile(filter_bam):
-        if args.dry_run:
-            sys.stderr.write('%s -m %d %s %s > %s\n' 
-                % (os.path.realpath(__file__), args.min_quality, args.target_bam, args.contam_bam, filter_bam))
-        else:
-            sys.stderr.write('Target: %s. Contamination: %s.\n'%(args.target_sam, args.contam_bam))
+        sys.stderr.write('%s -m %d %s %s > %s 2> %s\n' 
+                % (os.path.realpath(__file__), args.min_quality, args.target_bam, args.contam_bam, filter_bam, log_file))
+        if not args.dry_run:
+            #sys.stderr.write('Target: %s. Contamination: %s.\n'%(args.target_bam, args.contam_bam))
             min_qual = int(args.min_quality)
             
             # name-sort inputs
@@ -68,29 +80,50 @@ def main(args):
             fname = f.name
             f.close()
             with pysam.AlignmentFile(tname) as tfile, pysam.AlignmentFile(cname) as cfile:
-                filter_file = pysam.AlignmentFile(fname,'wb', template=tfile)
-                #contam_file = pysam.AlignmentFile(contam_bam,'wb', template=cfile)
-                #unmap_file = pysam.AlignmentFile(unmap_bam,'wb', template=tfile)
-                i = 0
+                filter_file = pysam.AlignmentFile(fname, 'wb', template=tfile)
+                tf = 0; cf = 0; i = 0; j = 0
+                tread = tfile.next()
+                treads = [tread]
+                cread = cfile.next()
+                creads = [cread]
+                if tread.query_name != cread.query_name:
+                    raise Exception('First read names in name-sorted BAMs do not match')
+                prev_query = tread.query_name
                 for tread in tfile:
-                    cread = cfile.next()
-                    assert tread.query_name == cread.query_name
-                    if tread.mapping_quality >= min_qual and tread.mapping_quality >= cread.mapping_quality:
-                        i += 1
-                        filter_file.write(tread)
-                    #if tread.mapping_quality < min_qual: # unmapped
-                    #    unmap_file.write(tread) # output mapping for target genome
-                    #elif tread.mapping_quality < cread.mapping_quality: # contamination
-                    #    contam_file.write(cread) # output mapping for contamination genome
-                    #else: #target
-                    #    filter_file.write(tread)
+                    if not tread.is_unmapped:
+                        tf += 1
+                    if tread.query_name == prev_query: # accumulate targets
+                        treads.append(tread)
+                    else: # analyze contam
+                        for cread in cfile:
+                            if not cread.is_unmapped:
+                                cf += 1
+                            if cread.query_name == prev_query: # accumulate contam
+                                    creads.append(cread)
+                            else:
+                                break
+                        (i, j) = compare_mapqs(treads, creads, filter_file, min_qual, i, j)
+                        treads = [tread]
+                        creads = [cread]
+                        prev_query = tread.query_name
+                for cread in cfile:
+                    cf += 1
+                    if cread.query_name == prev_query: # accumulate contam
+                        creads.append(cread)
+                    else:
+                        raise Exception('%s file has read %s not present in %s' % (args.contam_bam, cread.query_name, args.target_bam))
+                (i, j) = compare_mapqs(treads, creads, filter_file, min_qual, i, j)
                 filter_file.close()
-                #contam_file.close()
-                #unmap_file.close()
+                with open(log_file, 'w') as outfile:
+                    outfile.write('Target aligned fragments:\t%d\n' % (tf))
+                    outfile.write('Contam aligned fragments:\t%d\n' % (cf))
+                    outfile.write('Contamination Q%d fragments:\t%d\n' % (min_qual, j))
+                    outfile.write('Filtered Q%d fragments:\t%d\n' % (min_qual, i))
+                #sys.stderr.write('%d init target fragments %d init contam fragments %d filtered %d contam\n' % (tf, cf, i, j))
 
             # coordinate-sort output
             bam_sort(fname, filter_bam)
-            sys.stderr.write('Writing %d alignments to filtered output file %s.\n'%(i, filter_bam))
+            #sys.stderr.write('Writing %d alignments to filtered output file %s.\n'%(i, filter_bam))
             #sys.stdout.write('3 output files: filtered %s, contamination %s, unmapped %s\n'%(filter_bam, contam_bam, unmap_bam))
 
             #clean-up
