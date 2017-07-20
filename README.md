caf7be08f05462f10c9008930410e9ebdcf0beb9
Drevoz <alex.makunin@gmail.com>
2017-07-20 18:09:21 +0700

    vca_reg migration to GATK 4, leaving only basic procedures: prep, region variant calling, and annotation

--- README.md
+++ README.md
@@ -4,13 +4,13 @@ DOPseq_analyzer is a a set of tools for processing of sequencing data of isolate
 
 Currently, three pipelines are implemented: 
 
-1. Analysis of B chromosomes (pipeline/b_dopseq_pipe.py) includes read trimming, alignment to reference genome, contamination filtering, region calling and statistics calculation. Details on processing steps and additional routines for repetitive DNA characterization with RepeatExplorer and variant calling are described at the end of the configuration file (pipeline/b_dopseq_pipe.yaml).
+1. Analysis of B chromosomes (pipeline/b_dopseq_pipe.py) includes read trimming, alignment to reference genome, contamination filtering, region calling and statistics calculation.
 2. Analysis of anole microchromosomes (anolis/pipeline/anolis_dopseq_pipe.py) includes similar steps. It is optimized for reference genomes consisting of scaffolds and has a possibility to handle WGA libraries. Maintained by ilyakichigin.
 3. Variant calling for validated chromosome-specific regions with GATK HaplotypeCaller (pipeline/vca_reg.py).
 
 # Installation
 
-Dependancies:
+Dependencies:
 
 1. [cutadapt](http://cutadapt.readthedocs.io/en/stable/) (tested on v.1.8.3)
 
@@ -22,9 +22,10 @@ Dependancies:
 
 5. [DNAcopy](https://bioconductor.org/packages/release/bioc/html/DNAcopy.html) R (Bioconductor) package 
 
-6. (Optional - for subsequent variant calling and annotation) [GATK](https://software.broadinstitute.org/gatk/) (tested on v.3.3.0), [picard-tools](https://broadinstitute.github.io/picard/) (tested on v.1.125), [snpEff](http://snpeff.sourceforge.net/) (tested on v.3.3.0)
+6. (Optional - for subsequent variant calling and annotation) [GATK 4](https://software.broadinstitute.org/gatk/download/beta) (tested on beta.1), [snpEff](http://snpeff.sourceforge.net/) (tested on v.3.3.0)
 
-Package folder structure must be preserved in order for pipeline to find executable scripts. 
+
+After dependencies are installed, individual scrips and pipelines can be used. Note that internal folder structure must be preserved so that b_dopseq_pipe.py could find executable scripts. 
 
 # Usage 
 
@@ -34,5 +35,11 @@ Pipeline for chromosome region identification can be called with the command:
 ```
 Details on inputs, outputs and assignable parameters can be found in example `b_dopseq_pipe.yaml` config file.   Enabling dry run `-d` option provides command listing and checks if all the input files are present. 
 
-Briefly, this pipeline trims and aligns reads to the reference genome (and optonally potential contaminant genome), filters the alignment, and classifies selected chromosomes of the reference genome based on mean distances between mapped read positions. Regions with lower means can be further interpreted as specific to the isolated chromosomes. Note that these regions currently cannot be used 'as is' and require manual inspection and correction.
+Briefly, this pipeline trims and aligns reads to the reference genome (and optonally potential contaminant genome), filters the alignment, and classifies selected chromosomes of the reference genome based on mean distances between mapped read positions. Regions with lower means can be further interpreted as specific to the isolated chromosomes. Note that these regions cannot be used 'as is' and require manual inspection and correction.
 
+
+Pipeline for variant calling and annotation can be called similarly:
+```
+/some/folder/DOPseq_analyzer/pipeline/vca_reg.py [-d] vca_reg.yaml
+```
+Currently, this pipeline simply runs GATK HaplotypeCaller and snpEff annotation with default settings for a given set of regions (presumably present on the chromosome of interest). 
--- pipeline/vca_reg.py
+++ pipeline/vca_reg.py
@@ -9,103 +9,109 @@ import os
 import sys
 import yaml
 import shlex
+import argparse
 import subprocess
 
-def run_command(command, return_stdout = False, verbose = True):
+def parse_command_line_arguments():
+
+    parser = argparse.ArgumentParser(description=    
+                    """
+                    Pipeline for variant calling, filtering, annotation, and alternate reference generation f
+                    specified genome regions. 
+                    """
+                    )
+    parser.add_argument("config_file", help="input configuration file")
+    parser.add_argument("-d", "--dry_run", action="store_true", default=False,
+                        help="Check all dependencies and print out all commands")
+    
+    return parser.parse_args()
+
+def run_command(command, return_stdout = False, dry_run = True, verbose = True):
     if verbose:
-        print ' '.join(command)
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    (out, err) = process.communicate()
-    if process.returncode != 0:
-        sys.stderr.write(err)
-        sys.exit()
-    if return_stdout:
-        return out
+        print command
+    command = shlex.split(command)
+    if not dry_run:
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        (out, err) = process.communicate()
+        if process.returncode != 0:
+            sys.stderr.write(err)
+            sys.exit()
+        else:
+            with open('vca.log','a') as f:
+                f.write(err)
+        if return_stdout:
+            return out
 
-def run_java_program(jarfile, options, max_mem = "1g", return_stdout = False):
-    command = ['java', '-Xmx' + max_mem, '-jar', jarfile] + shlex.split(options) # shlex str split for correct quoted string interpretation
-    return run_command(command, return_stdout = return_stdout)
-
-def add_read_groups(bam, picard, samtools):
-    header = run_command([samtools, 'view', '-H', bam], return_stdout = True, verbose = False)
+def add_read_groups(bam, gatk, samtools, dry_run = True):
+    header = run_command(samtools + ' view -H ' + bam, return_stdout = True, dry_run = False, verbose = False)
     if '@RG' not in header:
         bam_prefix = bam.split('.')[0]
         temp_bam = bam + '.rg.tmp'
-        run_java_program(picard,
-            'AddOrReplaceReadGroups RGLB=1 RGPL=Illumina RGPU=1 RGSM=%s I=%s O=%s' % 
-            (bam_prefix, bam, temp_bam))
+        run_command('%s AddOrReplaceReadGroups --RGLB 1 --RGPL Illumina --RGPU 1 --RGSM %s -I %s -O %s'  
+            % (gatk, bam_prefix, bam, temp_bam), dry_run = dry_run)
         print 'mv %s %s' % (temp_bam, bam)
-        os.rename(temp_bam, bam)
+        if not dry_run:
+            os.rename(temp_bam, bam)
 
+def generate_names(aln, ref):
+
+    names = dict()
+    assert aln.endswith('.filter.bam')
+    names['pos'] = aln[:-11] + '.pos.bed'
+    names['fadict'] = '.'.join(ref.split('.')[:-1]) + '.dict'
+    names['md_bam'] = aln[:-4] + '.rmdup.bam'
+    names['md_log'] = aln[:-4] + '.rmdup.log'
+    names['vcf'] = aln[:-11] + '.reg.vcf'
+    names['ann_vcf'] = aln[:-11] + '.reg.ann.vcf'
+    names['ann_html'] = aln[:-11] + '.reg.ann.html'
+    return names
 
 # parse config
-with open(sys.argv[1]) as conf_file:
-    params = yaml.safe_load(conf_file)
-rg = params['ref_genome']
-sample = params['sample']
-samtools = params['samtools_path']
-picard = params['picard_path']
-gatk = params['gatk_path']
 
+args = parse_command_line_arguments()
+
+with open(args.config_file) as conf_file:
+    params = yaml.safe_load(conf_file)
+aln = params['bam_file']
+ref = params['ref_genome']
+reg = params['reg_bed']
+
+fnames = generate_names(aln, ref)
+
+### prepare
 # create sequence dictionary for genome
-fadict = '.'.join(rg.split('.')[:-1]) + '.dict'
-if not os.path.isfile(fadict):
-    run_java_program(picard,
-        'CreateSequenceDictionary R=%s O=%s' % (rg, fadict))
+if not os.path.isfile(fnames['fadict']):
+    run_command('%s CreateSequenceDictionary -R %s -O %s' 
+        % (params['gatk_path'], ref, fnames['fadict']), 
+        dry_run = args.dry_run)
 
 # index genome
-if not os.path.isfile(rg + '.fai'):
-        run_command([samtools, 'faidx', rg])
+if not os.path.isfile(ref + '.fai'):
+        run_command(params['samtools_path'] + ' faidx ' + ref, dry_run = args.dry_run)
 
+# mark duplicates
 
-for aln in params['bam_files']:
-    
-    # add read group data to BAM file
-    assert aln.endswith('.bam')
-    add_read_groups(aln, picard, samtools)
-    
-    # mark duplicates in BAM file
-    md_bam = aln[:-4] + '.rmdup.bam'
-    if not os.path.isfile(md_bam):
-        md_log = aln[:-4] + '.rmdup.txt'
-        run_java_program(picard,
-            'MarkDuplicates INPUT=%s OUTPUT=%s METRICS_FILE=%s' % (aln, md_bam, md_log))
-    
-    # index dedulpicated BAM file
-    if not os.path.isfile(md_bam + '.bai'):
-        run_command([samtools, 'index', md_bam])
+if not os.path.isfile(fnames['md_bam']):
+    add_read_groups(aln, params['gatk_path'], params['samtools_path'], dry_run = args.dry_run)
+    run_command('%s MarkDuplicates -I %s -O %s -M %s'
+     % (params['gatk_path'], aln, fnames['md_bam'], fnames['md_log']), dry_run = args.dry_run)
 
-    # infer genotypes at bp-resolution for target regions only
-    gvcf = aln[:-4] + '.g.vcf'
-    if not os.path.isfile(gvcf):
-        run_java_program(gatk, 
-            '-R %s -T HaplotypeCaller -I %s -ERC BP_RESOLUTION -L %s -o %s' % 
-                (rg, md_bam, params['reg_bed'], gvcf),
-            max_mem = params['gatk_mem'])
+# index dedulpicated BAM file
+if not os.path.isfile(fnames['md_bam'] + '.bai'):
+    run_command(params['samtools_path'] + ' index ' + fnames['md_bam'], dry_run = args.dry_run)
 
+###analyze
+# infer genotypes for verified regions only
+if not os.path.isfile(fnames['vcf']):
+    run_command('%s HaplotypeCaller -I %s -O %s -R %s -L %s' 
+        % (params['gatk_path'], fnames['md_bam'], fnames['vcf'], ref, reg), dry_run = args.dry_run)
 
-# call variants for all samples into one vcf
-vcf = sample + '.vcf'
-if not os.path.isfile(vcf):
-    gvcfs = ['--variant ' + aln[:-4] + '.gvcf' for aln in params['bam_files']]
-    run_java_program(gatk, 
-        '-R %s -T GenotypeGVCFs %s -o %s' % 
-            (rg, ' '.join(gvcfs), vcf),
-        max_mem = params['gatk_mem'])
-
-# filter variants
-vcf_filtered = sample + '.hq.vcf'
-if not os.path.isfile(vcf_filtered):
-    run_java_program(gatk, 
-        '-R %s -T SelectVariants %s --variant %s -o %s' % 
-            (rg, ' '.join(params['gatk_filters']), vcf, vcf_filtered))
-
-# annotate filtered variants
-vcf_annotated = sample + '.hq.ann.vcf'
-if not os.path.isfile(vcf_annotated):
-    ann_data = run_java_program(params['snpEff_path'],
-        '-s %s %s %s ' % (sample + '.hq.ann.summary.html', params['snpEff_genome'], vcf_filtered), 
-        return_stdout = True)
-    with open(vcf_annotated, 'w') as out:
-        print 'Writing to ' + vcf_annotated
-        out.write(ann_data)
+# annotate variants
+if not os.path.isfile(fnames['ann_vcf']):
+    ann_data = run_command('java -Xmx%s -jar %s -s %s %s %s ' 
+        % (params['max_mem'], params['snpEff_path'], fnames['ann_html'], params['snpEff_genome'], fnames['vcf']), 
+        return_stdout = True, dry_run = args.dry_run)
+    if not args.dry_run:
+        with open(fnames['ann_vcf'], 'w') as out:
+            print 'Writing to ' + fnames['ann_vcf']
+            out.write(ann_data)
